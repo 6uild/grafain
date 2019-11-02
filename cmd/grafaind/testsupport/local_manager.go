1c06d7348ecc6165556bc9a64646bece4cf0b89c
Alex Peters <alpe@users.noreply.github.com>
2019-11-02 21:20:35 +0100

    Fix test in non k8s setup

--- cmd/grafaind/testsupport/local_manager.go
+++ cmd/grafaind/testsupport/local_manager.go
@@ -1,20 +1,18 @@
 package testsupport
 
 import (
+	"fmt"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	k8runtime "sigs.k8s.io/controller-runtime/pkg/runtime/scheme"
 )
 
 func LocalManager() (manager.Manager, error) {
-	cfg, err := config.GetConfig()
-	if err != nil {
-		return nil, err
-	}
+
 	gv := schema.GroupVersion{Group: "", Version: "v1"}
 	s, err := (&k8runtime.Builder{GroupVersion: gv}).Register(&corev1.Pod{}, &corev1.PodList{}).Build()
 	if err != nil {
@@ -26,8 +24,12 @@ func LocalManager() (manager.Manager, error) {
 			return FakeMapper{}, nil
 		},
 	}
+	cfg := &rest.Config{Host:"example.com:80"}
 	mgr, err := manager.New(cfg, opts)
-	return mgr, err
+	if err != nil {
+		return nil, fmt.Errorf("failed to setup manager: %+v", err)
+	}
+	return mgr, nil
 }
 
 type FakeMapper struct {
